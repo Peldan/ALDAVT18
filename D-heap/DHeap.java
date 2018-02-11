74fa13d242b2000f03b33bda66f893be8517fdce
Arvid <peldans@gmail.com>
2018-02-11 22:00:08 +0100

    removed unnecessary comments

--- D-heap/DHeap.java
+++ D-heap/DHeap.java
@@ -1,48 +1,28 @@
-// Arvid Peld·n - arpe3186
-
-// Klassen i denna fil mÂste dˆpas om till DHeap fˆr att testerna ska fungera. 
+// Arvid Peld√°n - arpe3186
 package alda.heap;
 
 import java.util.Arrays;
-
-//DHeap class
-//
-//CONSTRUCTION: with optional capacity (that defaults to 100)
-//            or an array containing initial items
-//
-//******************PUBLIC OPERATIONS*********************
-//void insert( x )       --> Insert x
-//Comparable deleteMin( )--> Return and remove smallest item
-//Comparable findMin( )  --> Return smallest item
-//boolean isEmpty( )     --> Return true if empty; else false
-//void makeEmpty( )      --> Remove all items
-//******************ERRORS********************************
-//Throws UnderflowException as appropriate
 
 /**
  * Implements a binary heap. Note that all "matching" is based on the compareTo
  * method.
  * 
  * @author Mark Allen Weiss
+ * @author Arvid Peld√°n
  */
-public class DHeap<AnyType extends Comparable<? super AnyType>> {
-	/**
-	 * Construct the binary heap.
-	 */
 
+// modified Mark Allen Weiss' Binary Heap to a D-Heap
+
+	private static final int DEFAULT_DIMENSION = 2;
+	private static final int DEFAULT_CAPACITY = 10;
+	private int currentSize; 
 	private int d;
-	private static int DEFAULT_DIMENSION = 2;
+	private AnyType[] array; 
 
 	public DHeap() {
 		this(DEFAULT_DIMENSION);
 	}
 
-	/**
-	 * Construct the binary heap.
-	 * 
-	 * @param capacity
-	 *            the capacity of the binary heap.
-	 */
 	public DHeap(int d) {
 		this.d = d;
 		if (d < 2) {
@@ -52,13 +32,6 @@ public class DHeap<AnyType extends Comparable<? super AnyType>> {
 		array = (AnyType[]) new Comparable[d * DEFAULT_CAPACITY + 1];
 	}
 
-	/**
-	 * Insert into the priority queue, maintaining heap order. Duplicates are
-	 * allowed.
-	 * 
-	 * @param x
-	 *            the item to insert.
-	 */
 	public int parentIndex(int index) {
 		if (index < 2) {
 			throw new IllegalArgumentException();
@@ -86,22 +59,12 @@ public class DHeap<AnyType extends Comparable<? super AnyType>> {
 			array[i] = old[i];
 	}
 
-	/**
-	 * Find the smallest item in the priority queue.
-	 * 
-	 * @return the smallest item, or throw an UnderflowException if empty.
-	 */
 	public AnyType findMin() {
 		if (isEmpty())
 			throw new UnderflowException();
 		return array[1];
 	}
 
-	/**
-	 * Remove the smallest item from the priority queue.
-	 * 
-	 * @return the smallest item, or throw an UnderflowException if empty.
-	 */
 	public AnyType deleteMin() {
 		if (isEmpty())
 			throw new UnderflowException();
@@ -111,13 +74,6 @@ public class DHeap<AnyType extends Comparable<? super AnyType>> {
 		percolateDown(1);
 		return minItem;
 	}
-
-	/**
-	 * Internal method to percolate down in the heap.
-	 * 
-	 * @param hole
-	 *            the index at which the percolate begins.
-	 */
 
 	private void percolateDown(int hole) {
 		int child;
@@ -140,11 +96,6 @@ public class DHeap<AnyType extends Comparable<? super AnyType>> {
 		array[hole] = tmp;
 	}
 
-	/**
-	 * Establish heap order property from an arbitrary arrangement of items.
-	 * Runs in linear time.
-	 */
-
 	private void buildHeap() {
 		for (int i = currentSize / 2; i > 0; i--)
 			percolateDown(i);
@@ -158,28 +109,14 @@ public class DHeap<AnyType extends Comparable<? super AnyType>> {
 		return array[index];
 	}
 
-	/**
-	 * Test if the priority queue is logically empty.
-	 * 
-	 * @return true if empty, false otherwise.
-	 */
 	public boolean isEmpty() {
 		return currentSize == 0;
 	}
 
-	/**
-	 * Make the priority queue logically empty.
-	 */
 	public void makeEmpty() {
 		currentSize = 0;
 	}
 
-	private static final int DEFAULT_CAPACITY = 10;
-
-	private int currentSize; // Number of elements in heap
-	private AnyType[] array; // The heap array
-
-	// Test program
 	public static void main(String[] args) {
 		int numItems = 10000;
 		DHeap<Integer> h = new DHeap<>();
